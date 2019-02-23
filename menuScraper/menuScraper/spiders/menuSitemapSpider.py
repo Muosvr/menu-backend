b53e95b7e3882c8edda39c6c3f952ad21bde2ec9
Muosvr <lujason2015@gmail.com>
2019-02-22 22:52:27 -0500

    sitemap scraping

---
+++ menuScraper/menuScraper/spiders/menuSitemapSpider.py
@@ -0,0 +1,18 @@
+from scrapy.spiders import SitemapSpider
+
+
+class MySpider(SitemapSpider):
+    name = "sitemapSpider"
+    sitemap_urls = [
+        "https://www.myrecipes.com/sitemap-index.xml"]
+
+    def parse(self, response):
+        # with open("image_data.txt", "a+") as f:
+        for item in response.css("img"):
+            # f.write(response.urljoin(url) + "\n")
+            if item.css("::attr(src)").get() and item.css("::attr(alt)").get():
+                yield {
+                    'src': response.urljoin(item.css("::attr(src)").get()),
+                    'page_title': response.css("title::text").get(),
+                    'alt': response.urljoin(item.css("::attr(alt)").get())
+                }
